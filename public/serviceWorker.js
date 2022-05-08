c3c069d4092a1912dadbe374fcf343d93422a0e5
yuxianxu <yuxian.xu@gmail.com>
2022-05-07 22:35:53 -0700

    Update offline and manifest json file

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,35 @@
-const CACHE_NAME = "version-1";
+const CACHE_NAME = 'version-1';
 
 const urlstoCache = ['index.html', 'offline.html'];
 
+const self = this;
+
+//install service worker
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+
+      return cache.addAll(urlstoCache);
+    })
+  );
+
+  //Listen for requests
+  self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+      caches.keys().then((cacheNames) =>
+        Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        )
+      )
+    );
+  });
+});
